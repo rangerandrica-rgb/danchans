6fa85ede8375bd501a40b2b2bde0dbb248fd49e7
rangerandrica-rgb <rangerandrica@gmail.com>
2025-09-15 17:41:50 +0900

    Update danchan_app.py

--- danchan_app.py
+++ danchan_app.py
@@ -48,10 +48,9 @@ questions = st.session_state.questions
 
 if st.session_state.q_index < len(questions):
     q = st.session_state.questions[st.session_state.q_index]
-    st.write("画像パス:",q["image"])
-    img = Image.open(q["image"])
-    st.image(img, caption="このだんちゃんはどっち？", use_container_width=True)
-    choice = st.radio("答えを選んでください:", q["options"], key=f"q{st.session_state.q_index}")
+    st.image(q["image"], caption="このだんちゃんはどっち？", use_container_width=True)
+
+    choice = st．radio("答えを選んでください:", q["options"], key=f"q{st.session_state.q_index}")
 
     if not st.session_state.answered:
         if st.button("回答する"):
@@ -77,6 +76,7 @@ else:
         st.session_state.q_index = 0
         st.session_state.score = 0
         st.session_state.answered = False
+
 
 
 
