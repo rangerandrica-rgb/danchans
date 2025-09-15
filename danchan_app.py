8e0ced26b7a8e42642d8601ae84f0e4fcef5aef2
rangerandrica-rgb <rangerandrica@gmail.com>
2025-09-15 15:57:45 +0900

    Add files via upload

--- danchan_app.py
+++ danchan_app.py
@@ -17,7 +17,7 @@ questions = [
     {"image": "danchan/danchan9.jpg", "options": ["動物", "人間"], "answer": "人間"},
     {"image": "danchan/danchan10.jpg", "options": ["動物", "人間"], "answer": "人間"},
     {"image": "danchan/danchan11.jpg", "options": ["動物", "人間"], "answer": "人間"},
-    {"image": "danchan/danchan12.webp", "options": ["動物", "人間"], "answer": "人間"},
+    {"image": "danchan/danchan12.jpg", "options": ["動物", "人間"], "answer": "人間"},
     {"image": "danchan/danchan13.jpg", "options": ["動物", "人間"], "answer": "人間"},
     {"image": "danchan/danchan14.jpg", "options": ["動物", "人間"], "answer": "人間"},
     {"image": "danchan/dandan3.jpg", "options": ["動物", "人間"], "answer": "動物"},
